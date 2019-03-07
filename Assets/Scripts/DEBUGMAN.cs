4d714e79a04771bb1b6b6c2ce441f81a22836cb3
Louis <clouis466@gmail.com>
2019-03-07 10:41:48 +0100

    DebugSpaceBar

--- Assets/Scripts/DEBUGMAN.cs
+++ Assets/Scripts/DEBUGMAN.cs
@@ -3,20 +3,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DEBUGMAN : MonoBehaviour
 {
-    public InputsManagers im;
+    public UnityEvent down;
+    public UnityEvent up;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(13 / 3);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            down.Invoke();
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            up.Invoke();
+        }
     }
 }
