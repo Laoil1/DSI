068c635ff1306f951d40e297da58b56b85dfb468
Louis <clouis466@gmail.com>
2019-03-07 14:04:45 +0100

    Comit

---
+++ Assets/Scripts/Behaviors/Specifics/ChangeMatColorWithGradient.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMatColorWithGradient : MonoBehaviour
+{
+    public Gradient gradient;
+    public MeshRenderer mr;
+    public float speed=0.5f;
+    private float time;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        time += Time.deltaTime * speed;
+        if(time>=1)
+        {
+            time = 0;
+        }
+
+        mr.material.SetColor("_Color", gradient.Evaluate(time));
+    }
+}
